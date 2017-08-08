a0a9fe80c62346ae3132fe5e5a5269de2e9439c7
Serhii Gubenya <monoxrom@gmail.com>
2017-08-08 09:57:49 +0300

    A couple of new TODOs

--- lib/xml_builder.rb
+++ lib/xml_builder.rb
@@ -15,15 +15,17 @@ class XmlBuilder
 
   def get_api_credentials
       # TODO: it makes sense to move this logic to some other class due to its specific nature
-
+      # TODO: try to use headless browser here
       Capybara.current_driver = :selenium
 
       visit 'https://www.shopify.com/login'
 
+      # TODO: use env. vars here
       fill_in('Email', with: '***')
       fill_in('Password', with: '***')
       click_button('Log in')
 
+      # TODO: that works only for me, try to use redirect url after login
       visit 'https://finger-in-the-heaven.myshopify.com/admin/apps/private'
       
       table_row = page.all("#apps-private tr").last
